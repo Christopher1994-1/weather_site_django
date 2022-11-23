93b078c227e56fa9d00e90e32dd7c2431746137c
CEJ Van Niekirk <cejvanniekirk098@gmail.com>
2022-11-23 01:40:59 -0600

    added requests module to project

--- first_site/events/tests.py
+++ first_site/events/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from datetime import *
 from calendar import *
+from os import environ
+import requests
 
 # Create your tests here.
 now = datetime.now()
@@ -31,10 +33,23 @@ for day in weekdays.values():
 day = int(current_weekday) + 1
 next5days = list_weekdays[day:7]
 
-next_day1 = next5days[0]
-next_day2 = next5days[1]
-next_day3 = next5days[2]
-next_day4 = next5days[3]
-next_day5 = next5days[4]
+print(next5days)
 
-print(next_day1, next_day2, next_day3, next_day4, next_day5)
+# next_day1 = next5days[0]
+# next_day2 = next5days[1]
+# next_day3 = next5days[2]
+# next_day4 = next5days[3]
+# next_day5 = next5days[4]
+
+# print(next_day1, next_day2, next_day3, next_day4, next_day5)
+
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = environ.get("CEJ_Weather_API")
+CITY = "Arlington"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+
+response = requests.get(url).json()
+
+print(response)
